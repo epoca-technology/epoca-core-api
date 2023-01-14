23e42a32e44327126908ec4f00e7169bbe9e3877
JessDotJS <jessdotjs@gmail.com>
2023-01-14 12:31:01 -0400

    Market State Window change% adjustment

--- src/modules/market-state/window-state.service.ts
+++ src/modules/market-state/window-state.service.ts
@@ -23,8 +23,8 @@ export class WindowStateService implements IWindowStateService {
      * The percentage changes that must exist in the window in order for
      * it to have a state.
      */
-    private readonly minChange: number = 1;
-    private readonly strongChange: number = 5;
+    private readonly minChange: number = 1.5;
+    private readonly strongChange: number = 10;
 
 
 
